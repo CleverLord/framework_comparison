9af0c91cd3023501e8c370eaa9b01298e9575a2f
Krzysztof Łazarz <krzysztof.lazarz.dev@gmail.com>
2023-12-02 00:30:13 +0100

    Update doAllTests.sh

--- separate_dockers/unity-docker/doAllTests.sh
+++ separate_dockers/unity-docker/doAllTests.sh
@@ -1,16 +1,20 @@
 cd GameOfLife
 chmod +x run_all.sh
+chmod +x LinuxServerBuild.x86_64
 docker build -t gameoflife__unity_brainbuild .
 docker run gameoflife__unity_brainbuild
 cd ..
 
 cd Evacuation
 chmod +x run_all.sh
+chmod +x Evacuation_LinuxServerBuild.x86_64
 docker build -t evacuation__unity_brainbuild .
 docker run evacuation__unity_brainbuild
+cd ..
 
 cd Ants
 chmod +x run_all.sh
+chmod +x Ants_LinuxServerBuild.x86_64
 docker build -t ants__unity_brainbuild .
 docker run ants__unity_brainbuild
 cd ..
