f062730f0e0ada2d21f5f1a90ac2d0de5367ef74
Krzysztof Łazarz <krzysztof.lazarz.dev@gmail.com>
2023-12-02 13:19:50 +0100

    fixes

--- separate_dockers/unity-docker/doAllTests.sh
+++ separate_dockers/unity-docker/doAllTests.sh
@@ -1,20 +1,35 @@
+echo "Running GameOfLife"
 cd GameOfLife
+echo "setting permissions for .sh's for GameOfLife"
 chmod +x run_all_v2.sh
 chmod +x GameOfLife_LinuxServerBuild.x86_64
+echo "building GameOfLife docker image"
 docker build -t gameoflife__unity_brainbuild .
+echo "running GameOfLife docker image"
 docker run gameoflife__unity_brainbuild
+echo "done with GameOfLife"
 cd ..
 
+echo "Running Evacuation"
 cd Evacuation
+echo "setting permissions for .sh's for Evacuation"
 chmod +x run_all_v2.sh
 chmod +x Evacuation_LinuxServerBuild.x86_64
+echo "building Evacuation docker image"
 docker build -t evacuation__unity_brainbuild .
+echo "running Evacuation docker image"
 docker run evacuation__unity_brainbuild
+echo "done with Evacuation"
 cd ..
 
+echo "Running Ants"
 cd Ants
+echo "setting permissions for .sh's for Ants"
 chmod +x run_all_v2.sh
 chmod +x Ants_LinuxServerBuild.x86_64
+echo "building Ants docker image"
 docker build -t ants__unity_brainbuild .
+echo "running Ants docker image"
 docker run ants__unity_brainbuild
+echo "done with Ants"
 cd ..
